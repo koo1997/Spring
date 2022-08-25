61fd1677e71d234b9009f856010f86f8dc0b9d7b
yourname <ko90koa@gmail.com>
2022-08-25 18:07:22 +0900

    ㄴ

--- src/main/java/polymorphism/LgTV.java
+++ src/main/java/polymorphism/LgTV.java
@@ -1,12 +1,21 @@
 package polymorphism;
 
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 @Component("tv")
 public class LgTV implements TV {
-	@Autowired
-	@Qualifier("sony")
+//	@Autowired // 객채를 자동으로 할당
+//	@Qualifier("sony") //특정 객체의 이름을 이용하여 의존성 주입
+//	@Resource(name="apple")
+	
+	@Inject
+	@Named("sony")
 	private Speaker speaker;
 	
 	public LgTV() {
